277fe6a50dc5e33f5380e11cf581438cee1052d7
arihantjain124 <arihantjain124@outlook.com>
2022-02-25 16:51:13 +0530

    final commit before delete

--- read_nccn_graph_test.bat
+++ read_nccn_graph_test.bat
@@ -1,4 +1,4 @@
 
 
 ECHO Hello
-java -jar pdfbox-app-2.0.26-SNAPSHOT.jar ExtractText -rotationMagic -startPage 1 -endPage 10 nscl.pdf 1_10_page.txt
+java -jar pdfbox-app-2.0.26-SNAPSHOT.jar ExtractText -sort -startPage 14 -endPage 16 nscl.pdf 14_16_page.txt
